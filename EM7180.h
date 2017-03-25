e99e46298cd8ff56fff23d67381da9294097dffe
simondlevy <simon.d.levy@gmail.com>
2017-03-25 19:25:56 -0400

    =

--- EM7180.h
+++ EM7180.h
@@ -269,3 +269,139 @@ static void EM7180_begin(void)
     delay(1000); // give some time to read the screen
 
 }
+
+static float uint32_reg_to_float (uint8_t *buf)
+{
+    union {
+        uint32_t ui32;
+        float f;
+    } u;
+
+    u.ui32 =     (((uint32_t)buf[0]) +
+            (((uint32_t)buf[1]) <<  8) +
+            (((uint32_t)buf[2]) << 16) +
+            (((uint32_t)buf[3]) << 24));
+    return u.f;
+}
+static int16_t readSENtralBaroData()
+{
+    uint8_t rawData[2];  // x/y/z gyro register data stored here
+    readBytes(EM7180_ADDRESS, EM7180_Baro, 2, &rawData[0]);  // Read the two raw data registers sequentially into data array
+    return  (int16_t) (((int16_t)rawData[1] << 8) | rawData[0]);   // Turn the MSB and LSB into a signed 16-bit value
+}
+
+static int16_t readSENtralTempData()
+{
+    uint8_t rawData[2];  // x/y/z gyro register data stored here
+    readBytes(EM7180_ADDRESS, EM7180_Temp, 2, &rawData[0]);  // Read the two raw data registers sequentially into data array
+    return  (int16_t) (((int16_t)rawData[1] << 8) | rawData[0]);   // Turn the MSB and LSB into a signed 16-bit value
+}
+
+static void readSENtralQuatData(float * destination)
+{
+    uint8_t rawData[16];  // x/y/z quaternion register data stored here
+    readBytes(EM7180_ADDRESS, EM7180_QX, 16, &rawData[0]);       // Read the sixteen raw data registers into data array
+    destination[0] = uint32_reg_to_float (&rawData[0]);
+    destination[1] = uint32_reg_to_float (&rawData[4]);
+    destination[2] = uint32_reg_to_float (&rawData[8]);
+    destination[3] = uint32_reg_to_float (&rawData[12]);  // SENtral stores quats as qx, qy, qz, q0!
+
+}
+
+static void readSENtralAccelData(int16_t * destination)
+{
+    uint8_t rawData[6];  // x/y/z accel register data stored here
+    readBytes(EM7180_ADDRESS, EM7180_AX, 6, &rawData[0]);       // Read the six raw data registers into data array
+    destination[0] = (int16_t) (((int16_t)rawData[1] << 8) | rawData[0]);  // Turn the MSB and LSB into a signed 16-bit value
+    destination[1] = (int16_t) (((int16_t)rawData[3] << 8) | rawData[2]);  
+    destination[2] = (int16_t) (((int16_t)rawData[5] << 8) | rawData[4]); 
+}
+
+static void readSENtralGyroData(int16_t * destination)
+{
+    uint8_t rawData[6];  // x/y/z gyro register data stored here
+    readBytes(EM7180_ADDRESS, EM7180_GX, 6, &rawData[0]);  // Read the six raw data registers sequentially into data array
+    destination[0] = (int16_t) (((int16_t)rawData[1] << 8) | rawData[0]);   // Turn the MSB and LSB into a signed 16-bit value
+    destination[1] = (int16_t) (((int16_t)rawData[3] << 8) | rawData[2]);  
+    destination[2] = (int16_t) (((int16_t)rawData[5] << 8) | rawData[4]); 
+}
+
+static void readSENtralMagData(int16_t * destination)
+{
+    uint8_t rawData[6];  // x/y/z gyro register data stored here
+    readBytes(EM7180_ADDRESS, EM7180_MX, 6, &rawData[0]);  // Read the six raw data registers sequentially into data array
+    destination[0] = (int16_t) (((int16_t)rawData[1] << 8) | rawData[0]);   // Turn the MSB and LSB into a signed 16-bit value
+    destination[1] = (int16_t) (((int16_t)rawData[3] << 8) | rawData[2]);  
+    destination[2] = (int16_t) (((int16_t)rawData[5] << 8) | rawData[4]); 
+}
+
+static void EM7180_set_integer_param (uint8_t param, uint32_t param_val) {
+    uint8_t bytes[4], STAT;
+    bytes[0] = param_val & (0xFF);
+    bytes[1] = (param_val >> 8) & (0xFF);
+    bytes[2] = (param_val >> 16) & (0xFF);
+    bytes[3] = (param_val >> 24) & (0xFF);
+    param = param | 0x80; //Parameter is the decimal value with the MSB set high to indicate a paramter write processs
+    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte0, bytes[0]); //Param LSB
+    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte1, bytes[1]);
+    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte2, bytes[2]);
+    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte3, bytes[3]); //Param MSB
+    writeByte(EM7180_ADDRESS, EM7180_ParamRequest, param);
+    writeByte(EM7180_ADDRESS, EM7180_AlgorithmControl, 0x80); //Request parameter transfer procedure
+    STAT = readByte(EM7180_ADDRESS, EM7180_ParamAcknowledge); //Check the parameter acknowledge register and loop until the result matches parameter request byte
+    while(!(STAT==param)) {
+        STAT = readByte(EM7180_ADDRESS, EM7180_ParamAcknowledge);
+    }
+    writeByte(EM7180_ADDRESS, EM7180_ParamRequest, 0x00); //Parameter request = 0 to end parameter transfer process
+    writeByte(EM7180_ADDRESS, EM7180_AlgorithmControl, 0x00); // Re-start algorithm
+}
+
+int16_t readTempData()
+{
+    uint8_t rawData[2];  // x/y/z gyro register data stored here
+    readBytes(MPU9250_ADDRESS, TEMP_OUT_H, 2, &rawData[0]);  // Read the two raw data registers sequentially into data array 
+    return ((int16_t)rawData[0] << 8) | rawData[1] ;  // Turn the MSB and LSB into a 16-bit value
+}
+
+static void EM7180_set_mag_acc_FS (uint16_t mag_fs, uint16_t acc_fs) {
+    uint8_t bytes[4], STAT;
+    bytes[0] = mag_fs & (0xFF);
+    bytes[1] = (mag_fs >> 8) & (0xFF);
+    bytes[2] = acc_fs & (0xFF);
+    bytes[3] = (acc_fs >> 8) & (0xFF);
+    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte0, bytes[0]); //Mag LSB
+    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte1, bytes[1]); //Mag MSB
+    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte2, bytes[2]); //Acc LSB
+    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte3, bytes[3]); //Acc MSB
+    writeByte(EM7180_ADDRESS, EM7180_ParamRequest, 0xCA); //Parameter 74; 0xCA is 74 decimal with the MSB set high to indicate a paramter write processs
+    writeByte(EM7180_ADDRESS, EM7180_AlgorithmControl, 0x80); //Request parameter transfer procedure
+    STAT = readByte(EM7180_ADDRESS, EM7180_ParamAcknowledge); //Check the parameter acknowledge register and loop until the result matches parameter request byte
+    while(!(STAT==0xCA)) {
+        STAT = readByte(EM7180_ADDRESS, EM7180_ParamAcknowledge);
+    }
+    writeByte(EM7180_ADDRESS, EM7180_ParamRequest, 0x00); //Parameter request = 0 to end parameter transfer process
+    writeByte(EM7180_ADDRESS, EM7180_AlgorithmControl, 0x00); // Re-start algorithm
+}
+
+static void EM7180_set_gyro_FS (uint16_t gyro_fs) {
+    uint8_t bytes[4], STAT;
+    bytes[0] = gyro_fs & (0xFF);
+    bytes[1] = (gyro_fs >> 8) & (0xFF);
+    bytes[2] = 0x00;
+    bytes[3] = 0x00;
+    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte0, bytes[0]); //Gyro LSB
+    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte1, bytes[1]); //Gyro MSB
+    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte2, bytes[2]); //Unused
+    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte3, bytes[3]); //Unused
+    writeByte(EM7180_ADDRESS, EM7180_ParamRequest, 0xCB); //Parameter 75; 0xCB is 75 decimal with the MSB set high to indicate a paramter write processs
+    writeByte(EM7180_ADDRESS, EM7180_AlgorithmControl, 0x80); //Request parameter transfer procedure
+    STAT = readByte(EM7180_ADDRESS, EM7180_ParamAcknowledge); //Check the parameter acknowledge register and loop until the result matches parameter request byte
+    while(!(STAT==0xCB)) {
+        STAT = readByte(EM7180_ADDRESS, EM7180_ParamAcknowledge);
+    }
+    writeByte(EM7180_ADDRESS, EM7180_ParamRequest, 0x00); //Parameter request = 0 to end parameter transfer process
+    writeByte(EM7180_ADDRESS, EM7180_AlgorithmControl, 0x00); // Re-start algorithm
+}
+
+
+
