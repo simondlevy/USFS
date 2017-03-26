039c597a84a44821f8a0949899e0765f6bb2de2f
Simon D. Levy <simon.d.levy@gmail.com>
2017-03-26 01:49:11 -0400

    Cleanup

--- EM7180.cpp
+++ EM7180.cpp
@@ -104,50 +104,6 @@
 #define AK8963_ADDRESS           0x0C   // Address of magnetometer
 #define BMP280_ADDRESS           0x76   // Address of BMP280 altimeter when ADO = 0
 
-enum Posr {
-    P_OSR_00 = 0,  // no op
-    P_OSR_01,
-    P_OSR_02,
-    P_OSR_04,
-    P_OSR_08,
-    P_OSR_16
-};
-
-enum Tosr {
-    T_OSR_00 = 0,  // no op
-    T_OSR_01,
-    T_OSR_02,
-    T_OSR_04,
-    T_OSR_08,
-    T_OSR_16
-};
-
-enum IIRFilter {
-    full = 0,  // bandwidth at full sample rate
-    BW0_223ODR,
-    BW0_092ODR,
-    BW0_042ODR,
-    BW0_021ODR // bandwidth at 0.021 x sample rate
-};
-
-enum Mode {
-    BMP280Sleep = 0,
-    forced,
-    forced2,
-    normal
-};
-
-enum SBy {
-    t_00_5ms = 0,
-    t_62_5ms,
-    t_125ms,
-    t_250ms,
-    t_500ms,
-    t_1000ms,
-    t_2000ms,
-    t_4000ms,
-};
-
 // I2C read/write functions for the MPU9250 and AK8963 sensors
 
 static void writeByte(uint8_t address, uint8_t subAddress, uint8_t data)
--- examples/Normal/Normal.ino
+++ examples/Normal/Normal.ino
@@ -78,7 +78,6 @@ void setup()
 
 }
 
-
 void loop()
 {  
     static int count, sumCount;
