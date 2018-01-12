e7ddd9d92cd5fa4c06965729bd6e8901f375258e
simondlevy <simon.d.levy@gmail.com>
2018-01-11 20:10:56 -0500

    Moar cleanup

--- examples/Interrupt/Interrupt.ino
+++ examples/Interrupt/Interrupt.ino
@@ -35,76 +35,14 @@
 EM7180 em7180;
 
 
-// EM7180 SENtral register map
-// see http://www.emdeveloper.com/downloads/7180/EMSentral_EM7180_Register_Map_v1_3.pdf
-//
 #define EM7180_QX                 0x00  // this is a 32-bit normalized floating point number read from registers 0x00-03
-#define EM7180_QY                 0x04  // this is a 32-bit normalized floating point number read from registers 0x04-07
-#define EM7180_QZ                 0x08  // this is a 32-bit normalized floating point number read from registers 0x08-0B
-#define EM7180_QW                 0x0C  // this is a 32-bit normalized floating point number read from registers 0x0C-0F
-#define EM7180_QTIME              0x10  // this is a 16-bit unsigned integer read from registers 0x10-11
-#define EM7180_MX                 0x12  // int16_t from registers 0x12-13
-#define EM7180_MY                 0x14  // int16_t from registers 0x14-15
-#define EM7180_MZ                 0x16  // int16_t from registers 0x16-17
-#define EM7180_MTIME              0x18  // uint16_t from registers 0x18-19
 #define EM7180_AX                 0x1A  // int16_t from registers 0x1A-1B
-#define EM7180_AY                 0x1C  // int16_t from registers 0x1C-1D
-#define EM7180_AZ                 0x1E  // int16_t from registers 0x1E-1F
-#define EM7180_ATIME              0x20  // uint16_t from registers 0x20-21
 #define EM7180_GX                 0x22  // int16_t from registers 0x22-23
-#define EM7180_GY                 0x24  // int16_t from registers 0x24-25
-#define EM7180_GZ                 0x26  // int16_t from registers 0x26-27
-#define EM7180_GTIME              0x28  // uint16_t from registers 0x28-29
+#define EM7180_MX                 0x12  // int16_t from registers 0x12-13
 #define EM7180_Baro               0x2A  // start of two-byte MS5637 pressure data, 16-bit signed interger
-#define EM7180_BaroTIME           0x2C  // start of two-byte MS5637 pressure timestamp, 16-bit unsigned
 #define EM7180_Temp               0x2E  // start of two-byte MS5637 temperature data, 16-bit signed interger
-#define EM7180_TempTIME           0x30  // start of two-byte MS5637 temperature timestamp, 16-bit unsigned
-#define EM7180_QRateDivisor       0x32  // uint8_t 
-#define EM7180_EnableEvents       0x33
-#define EM7180_HostControl        0x34
 #define EM7180_EventStatus        0x35
-#define EM7180_SensorStatus       0x36
-#define EM7180_SentralStatus      0x37
-#define EM7180_AlgorithmStatus    0x38
-#define EM7180_FeatureFlags       0x39
-#define EM7180_ParamAcknowledge   0x3A
-#define EM7180_SavedParamByte0    0x3B
-#define EM7180_SavedParamByte1    0x3C
-#define EM7180_SavedParamByte2    0x3D
-#define EM7180_SavedParamByte3    0x3E
-#define EM7180_ActualMagRate      0x45
-#define EM7180_ActualAccelRate    0x46
-#define EM7180_ActualGyroRate     0x47
-#define EM7180_ActualBaroRate     0x48
-#define EM7180_ActualTempRate     0x49
 #define EM7180_ErrorRegister      0x50
-#define EM7180_AlgorithmControl   0x54
-#define EM7180_MagRate            0x55
-#define EM7180_AccelRate          0x56
-#define EM7180_GyroRate           0x57
-#define EM7180_BaroRate           0x58
-#define EM7180_TempRate           0x59
-#define EM7180_LoadParamByte0     0x60
-#define EM7180_LoadParamByte1     0x61
-#define EM7180_LoadParamByte2     0x62
-#define EM7180_LoadParamByte3     0x63
-#define EM7180_ParamRequest       0x64
-#define EM7180_ROMVersion1        0x70
-#define EM7180_ROMVersion2        0x71
-#define EM7180_RAMVersion1        0x72
-#define EM7180_RAMVersion2        0x73
-#define EM7180_ProductID          0x90
-#define EM7180_RevisionID         0x91
-#define EM7180_RunStatus          0x92
-#define EM7180_UploadAddress      0x94 // uint16_t registers 0x94 (MSB)-5(LSB)
-#define EM7180_UploadData         0x96  
-#define EM7180_CRCHost            0x97  // uint32_t from registers 0x97-9A
-#define EM7180_ResetRequest       0x9B   
-#define EM7180_PassThruStatus     0x9E   
-#define EM7180_PassThruControl    0xA0
-#define EM7180_ACC_LPF_BW         0x5B  //Register GP36
-#define EM7180_GYRO_LPF_BW        0x5C  //Register GP37
-#define EM7180_BARO_LPF_BW        0x5D  //Register GP38
 
 #define EM7180_ADDRESS           0x28   // Address of the EM7180 SENtral sensor hub
 
@@ -125,14 +63,6 @@ static uint16_t EM7180_mag_fs, EM7180_acc_fs, EM7180_gyro_fs; // EM7180 sensor f
 static float ax, ay, az, gx, gy, gz, mx, my, mz; // variables to hold latest sensor data values 
 
 // I2C read/write functions for the MPU9250 and AK8963 sensors
-
-static void writeByte(uint8_t address, uint8_t subAddress, uint8_t data)
-{
-    Wire.beginTransmission(address);  // Initialize the Tx buffer
-    Wire.write(subAddress);           // Put slave register address in Tx buffer
-    Wire.write(data);                 // Put data in Tx buffer
-    Wire.endTransmission();           // Send the Tx buffer
-}
 
 static uint8_t readByte(uint8_t address, uint8_t subAddress)
 {
@@ -176,67 +106,6 @@ static float uint32_reg_to_float (uint8_t *buf)
             (((uint32_t)buf[2]) << 16) +
             (((uint32_t)buf[3]) << 24));
     return u.f;
-}
-
-static void EM7180_set_gyro_FS (uint16_t gyro_fs) {
-    uint8_t bytes[4], STAT;
-    bytes[0] = gyro_fs & (0xFF);
-    bytes[1] = (gyro_fs >> 8) & (0xFF);
-    bytes[2] = 0x00;
-    bytes[3] = 0x00;
-    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte0, bytes[0]); //Gyro LSB
-    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte1, bytes[1]); //Gyro MSB
-    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte2, bytes[2]); //Unused
-    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte3, bytes[3]); //Unused
-    writeByte(EM7180_ADDRESS, EM7180_ParamRequest, 0xCB); //Parameter 75; 0xCB is 75 decimal with the MSB set high to indicate a paramter write processs
-    writeByte(EM7180_ADDRESS, EM7180_AlgorithmControl, 0x80); //Request parameter transfer procedure
-    STAT = readByte(EM7180_ADDRESS, EM7180_ParamAcknowledge); //Check the parameter acknowledge register and loop until the result matches parameter request byte
-    while(!(STAT==0xCB)) {
-        STAT = readByte(EM7180_ADDRESS, EM7180_ParamAcknowledge);
-    }
-    writeByte(EM7180_ADDRESS, EM7180_ParamRequest, 0x00); //Parameter request = 0 to end parameter transfer process
-    writeByte(EM7180_ADDRESS, EM7180_AlgorithmControl, 0x00); // Re-start algorithm
-}
-
-static void EM7180_set_mag_acc_FS (uint16_t mag_fs, uint16_t acc_fs) {
-    uint8_t bytes[4], STAT;
-    bytes[0] = mag_fs & (0xFF);
-    bytes[1] = (mag_fs >> 8) & (0xFF);
-    bytes[2] = acc_fs & (0xFF);
-    bytes[3] = (acc_fs >> 8) & (0xFF);
-    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte0, bytes[0]); //Mag LSB
-    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte1, bytes[1]); //Mag MSB
-    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte2, bytes[2]); //Acc LSB
-    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte3, bytes[3]); //Acc MSB
-    writeByte(EM7180_ADDRESS, EM7180_ParamRequest, 0xCA); //Parameter 74; 0xCA is 74 decimal with the MSB set high to indicate a paramter write processs
-    writeByte(EM7180_ADDRESS, EM7180_AlgorithmControl, 0x80); //Request parameter transfer procedure
-    STAT = readByte(EM7180_ADDRESS, EM7180_ParamAcknowledge); //Check the parameter acknowledge register and loop until the result matches parameter request byte
-    while(!(STAT==0xCA)) {
-        STAT = readByte(EM7180_ADDRESS, EM7180_ParamAcknowledge);
-    }
-    writeByte(EM7180_ADDRESS, EM7180_ParamRequest, 0x00); //Parameter request = 0 to end parameter transfer process
-    writeByte(EM7180_ADDRESS, EM7180_AlgorithmControl, 0x00); // Re-start algorithm
-}
-
-static void EM7180_set_integer_param (uint8_t param, uint32_t param_val) {
-    uint8_t bytes[4], STAT;
-    bytes[0] = param_val & (0xFF);
-    bytes[1] = (param_val >> 8) & (0xFF);
-    bytes[2] = (param_val >> 16) & (0xFF);
-    bytes[3] = (param_val >> 24) & (0xFF);
-    param = param | 0x80; //Parameter is the decimal value with the MSB set high to indicate a paramter write processs
-    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte0, bytes[0]); //Param LSB
-    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte1, bytes[1]);
-    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte2, bytes[2]);
-    writeByte(EM7180_ADDRESS, EM7180_LoadParamByte3, bytes[3]); //Param MSB
-    writeByte(EM7180_ADDRESS, EM7180_ParamRequest, param);
-    writeByte(EM7180_ADDRESS, EM7180_AlgorithmControl, 0x80); //Request parameter transfer procedure
-    STAT = readByte(EM7180_ADDRESS, EM7180_ParamAcknowledge); //Check the parameter acknowledge register and loop until the result matches parameter request byte
-    while(!(STAT==param)) {
-        STAT = readByte(EM7180_ADDRESS, EM7180_ParamAcknowledge);
-    }
-    writeByte(EM7180_ADDRESS, EM7180_ParamRequest, 0x00); //Parameter request = 0 to end parameter transfer process
-    writeByte(EM7180_ADDRESS, EM7180_AlgorithmControl, 0x00); // Re-start algorithm
 }
 
 static void readSENtralQuatData(float * destination)
