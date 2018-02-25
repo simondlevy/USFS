154f3f4653b2df578254d794a6cf518289f0cf14
simondlevy <simon.d.levy@gmail.com>
2018-02-24 22:26:47 -0500

    Cleanup

--- examples/RateTest/RateTest.ino
+++ examples/RateTest/RateTest.ino
@@ -42,9 +42,9 @@ void setup()
     Serial.begin(38400);
 
     // Initialize the EM7180 ODRs
-    em7180.accelRate = 300;
-    em7180.gyroRate = 300;
-    em7180.baroRate = 40;
+    em7180.accelRate = 330;
+    em7180.gyroRate = 330;
+    em7180.baroRate = 50;
     em7180.qRateDivisor = 5;
 
     // Start the EM7180 in master mode
