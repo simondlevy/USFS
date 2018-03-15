8b28178b5b579c1ef5b60ef2fcbcb44c72d47371
simondlevy <simon.d.levy@gmail.com>
2018-03-15 16:39:50 -0400

    Begin using quaternions to rotate accelerometer into earth frame

--- examples/BaroImuFusion/BaroImuFusion.ino
+++ examples/BaroImuFusion/BaroImuFusion.ino
@@ -1,6 +1,8 @@
 /* 
    BaroImuFusion.ino: altitude estimation via barometer / IMU fusion
 
+   Copyright (C) 2018 Simon D. Levy and Greg Tomasch
+
    This file is part of EM7180.
 
    EM7180 is free software: you can redistribute it and/or modify
@@ -26,8 +28,56 @@
 #define NOSTOP false
 #endif
 
-
 EM7180 em7180;
+
+// 1G = 2048 accelerometer reading
+static float G_PER_COUNT = 1/2048.f;
+
+// Accelerometer raw (ADC) and quaternions will be set in loop as readings become available
+static int16_t accADC[3];
+static float qt[4];
+
+static void computeIMU ()
+{
+    // Pass-thru for future filter experimentation
+    int16_t accSmooth[3];
+    accSmooth[0] = accADC[0];
+    accSmooth[1] = accADC[1];
+    accSmooth[2] = accADC[2];
+
+    // Rotation matrix components to calculate Euler anfles and linear acceleration
+    float a11 = qt[0]*qt[0]+qt[1]*qt[1] -qt[2]*qt[2]-qt[3]*qt[3];
+    float a12 = 2.0f*(qt[1]*qt[2]-qt[0]*qt[3]);
+    float a13 = 2.0f*(qt[1]*qt[3]+qt[0]*qt[2]);
+    float a21 = 2.0f*(qt[0]*qt[3]+qt[1]*qt[2]);
+    float a22 = qt[0]*qt[0]-qt[1]*qt[1] +qt[2]*qt[2]-qt[3]*qt[3];
+    float a23 = 2.0f*(qt[2]*qt[3]-qt[0]*qt[1]);
+    float a31 = 2.0f*(qt[1]*qt[3]-qt[0]*qt[2]);
+    float a32 = 2.0f*(qt[0]*qt[1]+qt[2]*qt[3]);
+    float a33 = qt[0]*qt[0]-qt[1]*qt[1] -qt[2]*qt[2]+qt[3]*qt[3];
+
+    float acc_X_minus_grav = (float)accSmooth[0];
+    float acc_Y_minus_grav = (float)accSmooth[1];
+    float acc_Z_minus_grav = (float)accSmooth[2];
+
+    acc_X_minus_grav *= G_PER_COUNT;
+    acc_Y_minus_grav *= G_PER_COUNT;
+    acc_Z_minus_grav *= G_PER_COUNT;
+
+    Serial.print(acc_Z_minus_grav);
+
+    acc_X_minus_grav -= a31;
+    acc_Y_minus_grav -= a32;
+    acc_Z_minus_grav -= a33;
+
+    Serial.print(" => ");
+    Serial.println(acc_Z_minus_grav);
+
+    float LINaccData[3];
+    LINaccData[0] = -(a11*acc_X_minus_grav + a12*acc_Y_minus_grav + a13*acc_Z_minus_grav); // 0=LAT 
+    LINaccData[1] = -(a21*acc_X_minus_grav + a22*acc_Y_minus_grav + a23*acc_Z_minus_grav); // 1=LON
+    LINaccData[2] =  (a31*acc_X_minus_grav + a32*acc_Y_minus_grav + a33*acc_Z_minus_grav); // 2=Z
+}
 
 void setup()
 {
@@ -67,61 +117,17 @@ void loop()
     }
 
     if (em7180.gotQuaternions()) {
-
-        float q[4];
-
-        em7180.readQuaternions(q);
-
-        float yaw   = atan2(2.0f * (q[0] * q[1] + q[3] * q[2]), q[3] * q[3] + q[0] * q[0] - q[1] * q[1] - q[2] * q[2]);   
-        float pitch = -asin(2.0f * (q[0] * q[2] - q[3] * q[1]));
-        float roll  = atan2(2.0f * (q[3] * q[0] + q[1] * q[2]), q[3] * q[3] - q[0] * q[0] - q[1] * q[1] + q[2] * q[2]);
-        pitch *= 180.0f / PI;
-        yaw   *= 180.0f / PI; 
-        yaw   += 13.8f; // Declination at Danville, California is 13 degrees 48 minutes and 47 seconds on 2014-04-04
-        if(yaw < 0) yaw   += 360.0f ; // Ensure yaw stays between 0 and 360
-        roll  *= 180.0f / PI;
-
-        Serial.print("Quaternion Yaw, Pitch, Roll: ");
-        Serial.print(yaw, 2);
-        Serial.print(", ");
-        Serial.print(pitch, 2);
-        Serial.print(", ");
-        Serial.println(roll, 2);
+        em7180.readQuaternions(qt);
     }
 
     if (em7180.gotAccelerometer()) {
-        int16_t a[3];
-        em7180.readAccelerometer(a);
-        Serial.print("Accel: ");
-        Serial.print(a[0]);
-        Serial.print(", ");
-        Serial.print(a[1]);
-        Serial.print(", ");
-        Serial.println(a[2]);
+        em7180.readAccelerometer(accADC);
+        computeIMU();
     }
 
-    if (em7180.gotGyrometer()) {
-        int16_t g[3];
-        em7180.readGyrometer(g);
-        Serial.print("Gyro: ");
-        Serial.print(g[0]);
-        Serial.print(", ");
-        Serial.print(g[1]);
-        Serial.print(", ");
-        Serial.println(g[2]);
-    }
-
-
-    if (em7180.gotBarometer()) 
-    {
+    if (em7180.gotBarometer()) {
         float temperature, pressure;
-
         em7180.readBarometer(pressure, temperature);
-
-        float altitude = (1.0f - powf(pressure / 1013.25f, 0.190295f)) * 44330.0f;
-        Serial.print("Baro Altitude = "); 
-        Serial.print(altitude, 2); 
-        Serial.println(" m\n");
     }
 }
 
