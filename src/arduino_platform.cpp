970f58611245bb69962ba3500a70d7a6d74ad6de
Bartosz Meglicki <bmegli@op.pl>
2018-07-17 21:35:17 +0200

    add support for Teensy LC, Teensy 3.5 and Teensy 3.6
    Use i2c_t3 library and I2C_NOSTOP for Teensy boards.
    
    This commit extends #ifdef for Teensy 3.1/3.2
    to all Teensy boards supported by i2c_t3.
    
    For discussion on Teensy board detection see:
    https://forum.pjrc.com/threads/46694-Board-Specific-Compilation

--- src/arduino_platform.cpp
+++ src/arduino_platform.cpp
@@ -20,7 +20,9 @@
 #include "cross_platform.h"
 
 #include <Arduino.h>
-#ifdef __MK20DX256__
+
+//Teensy 3.1/3.2 (__MK20DX256__), Teensy LC (__MKL26Z64__) Teensy 3.5 (__MK64FX512__), Teensy 3.6 (__MK66FX1M0__)
+#if defined(CORE_TEENSY) && ( defined(__MK20DX256__) || defined(__MKL26Z64__) || defined(__MK64FX512__) || defined(__MK66FX1M0__) )
 #include <i2c_t3.h>
 #define NOSTOP I2C_NOSTOP
 #else
