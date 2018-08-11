e49d5fba26dbf50b0dd4370eaeb7eac70e00399d
simondlevy <simon.d.levy@gmail.com>
2018-08-11 16:33:31 -0400

    Uses MPU9250 library instead of raw registers

--- examples/PassThru/PassThru.ino
+++ examples/PassThru/PassThru.ino
@@ -1,10 +1,6 @@
 /* 
    PassThru.ino: Example sketch for running EM7180 SENtral sensor hub in pass-through mode.
 
-   Adapted from
-
-     https://raw.githubusercontent.com/kriswiner/Teensy_Flight_Controller/master/em7180.MPU9250_BMP280
-
    This file is part of EM7180.
 
    EM7180 is free software: you can redistribute it and/or modify
@@ -21,39 +17,7 @@
  */
 
 #include "EM7180.h"
-
-// See also MPU-9250 Register Map and Descriptions, Revision 4.0, RM-MPU-9250A-00, Rev. 1.4, 9/9/2013 for registers not listed in 
-// above document; the MPU9250 and MPU9150 are virtually identical but the latter has a different register map
-
-#define CONFIG           0x1A
-#define GYRO_CONFIG      0x1B
-#define ACCEL_CONFIG     0x1C
-#define ACCEL_CONFIG2    0x1D
-#define SMPLRT_DIV       0x19
-
-#define INT_PIN_CFG      0x37
-#define INT_ENABLE       0x38
-#define DMP_INT_STATUS   0x39  // Check DMP interrupt
-#define INT_STATUS       0x3A
-
-#define ACCEL_XOUT_H     0x3B
-#define ACCEL_XOUT_L     0x3C
-#define ACCEL_YOUT_H     0x3D
-#define ACCEL_YOUT_L     0x3E
-#define ACCEL_ZOUT_H     0x3F
-#define ACCEL_ZOUT_L     0x40
-
-#define GYRO_XOUT_H      0x43
-#define GYRO_XOUT_L      0x44
-#define GYRO_YOUT_H      0x45
-#define GYRO_YOUT_L      0x46
-#define GYRO_ZOUT_H      0x47
-#define GYRO_ZOUT_L      0x48
-
-#define PWR_MGMT_1       0x6B // Device defaults to the SLEEP mode
-#define WHO_AM_I_MPU9250 0x75 // Should return 0x71
-
-#define MPU9250_ADDRESS          0x68   // Device address of MPU9250 when ADO = 0
+#include <MPU9250.h>
 
 #ifdef __MK20DX256__
 #include <i2c_t3.h>
@@ -63,135 +27,50 @@
 #define NOSTOP false
 #endif
 
-
-enum Ascale {
-    AFS_2G = 0,
-    AFS_4G,
-    AFS_8G,
-    AFS_16G
-};
-
-enum Gscale {
-    GFS_250DPS = 0,
-    GFS_500DPS,
-    GFS_1000DPS,
-    GFS_2000DPS
-};
-
-// I2C read/write functions for the MPU9250
-
-static void _writeByte(uint8_t address, uint8_t subAddress, uint8_t data)
-{
-    Wire.beginTransmission(address);  // Initialize the Tx buffer
-    Wire.write(subAddress);           // Put slave register address in Tx buffer
-    Wire.write(data);                 // Put data in Tx buffer
-    Wire.endTransmission();           // Send the Tx buffer
-}
-
-static uint8_t _readByte(uint8_t address, uint8_t subAddress)
-{
-    uint8_t data; // `data` will store the register data	 
-    Wire.beginTransmission(address);         // Initialize the Tx buffer
-    Wire.write(subAddress);	                 // Put slave register address in Tx buffer
-    Wire.endTransmission(NOSTOP);        // Send the Tx buffer, but send a restart to keep connection alive
-    Wire.requestFrom(address, (size_t) 1);   // Read one byte from slave register address 
-    data = Wire.read();                      // Fill Rx buffer with result
-    return data;                             // Return data read from slave register
-}
-
-static void _readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * dest)
-{  
-    Wire.beginTransmission(address);   // Initialize the Tx buffer
-    Wire.write(subAddress);            // Put slave register address in Tx buffer
-    Wire.endTransmission(NOSTOP);  // Send the Tx buffer, but send a restart to keep connection alive
-    uint8_t i = 0;
-    Wire.requestFrom(address, (size_t) count);  // Read bytes from slave register address 
-    while (Wire.available()) {
-        dest[i++] = Wire.read(); }         // Put read results in the Rx buffer
-}
-
-
-static void initMPU9250(uint8_t ascale, uint8_t gscale)
-{  
-    // wake up device
-    _writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x00); // Clear sleep mode bit (6), enable all sensors 
-    delay(100); // Wait for all registers to reset 
-
-    // get stable time source
-    _writeByte(MPU9250_ADDRESS, PWR_MGMT_1, 0x01);  // Auto select clock source to be PLL gyroscope reference if ready else
-    delay(200); 
-
-    // Configure Gyro and Thermometer
-    // Disable FSYNC and set thermometer and gyro bandwidth to 41 and 42 Hz, respectively; 
-    // minimum delay time for this setting is 5.9 ms, which means sensor fusion update rates cannot
-    // be higher than 1 / 0.0059 = 170 Hz
-    // DLPF_CFG = bits 2:0 = 011; this limits the sample rate to 1000 Hz for both
-    // With the MPU9250, it is possible to get gyro sample rates of 32 kHz (!), 8 kHz, or 1 kHz
-    _writeByte(MPU9250_ADDRESS, CONFIG, 0x03);  
-
-    // Set sample rate = gyroscope output rate/(1 + SMPLRT_DIV)
-    _writeByte(MPU9250_ADDRESS, SMPLRT_DIV, 0x04);  // Use a 200 Hz rate; a rate consistent with the filter update rate 
-    // determined inset in CONFIG above
-
-    // Set gyroscope full scale range
-    // Range selects FS_SEL and AFS_SEL are 0 - 3, so 2-bit values are left-shifted into positions 4:3
-    uint8_t c = _readByte(MPU9250_ADDRESS, GYRO_CONFIG); // get current GYRO_CONFIG register value
-    // c = c & ~0xE0; // Clear self-test bits [7:5] 
-    c = c & ~0x02; // Clear Fchoice bits [1:0] 
-    c = c & ~0x18; // Clear AFS bits [4:3]
-    c = c | gscale << 3; // Set full scale range for the gyro
-    _writeByte(MPU9250_ADDRESS, GYRO_CONFIG, c ); // Write new GYRO_CONFIG value to register
-
-    // Set accelerometer full-scale range configuration
-    c = _readByte(MPU9250_ADDRESS, ACCEL_CONFIG); // get current ACCEL_CONFIG register value
-    c = c & ~0x18;  // Clear AFS bits [4:3]
-    c = c | ascale << 3; // Set full scale range for the accelerometer 
-    _writeByte(MPU9250_ADDRESS, ACCEL_CONFIG, c); // Write new ACCEL_CONFIG register value
-
-    // Set accelerometer sample rate configuration
-    // It is possible to get a 4 kHz sample rate from the accelerometer by choosing 1 for
-    // accel_fchoice_b bit [3]; in this case the bandwidth is 1.13 kHz
-    c = _readByte(MPU9250_ADDRESS, ACCEL_CONFIG2); // get current ACCEL_CONFIG2 register value
-    c = c & ~0x0F; // Clear accel_fchoice_b (bit 3) and A_DLPFG (bits [2:0])  
-    c = c | 0x03;  // Set accelerometer rate to 1 kHz and bandwidth to 41 Hz
-    _writeByte(MPU9250_ADDRESS, ACCEL_CONFIG2, c); // Write new ACCEL_CONFIG2 register value
-
-    // The accelerometer, gyro, and thermometer are set to 1 kHz sample rates, 
-    // but all these rates are further reduced by a factor of 5 to 200 Hz because of the SMPLRT_DIV setting
-
-    // Configure Interrupts and Bypass Enable
-    // Set interrupt pin active high, push-pull, hold interrupt pin level HIGH until interrupt cleared,
-    // clear on read of INT_STATUS, and enable I2C_BYPASS_EN so additional chips 
-    // can join the I2C bus and all can be controlled by the Arduino as master
-    _writeByte(MPU9250_ADDRESS, INT_PIN_CFG, 0x22);    
-    _writeByte(MPU9250_ADDRESS, INT_ENABLE, 0x01);  // Enable data ready (bit 0) interrupt
-    delay(100);
-}
-
-
-static void readGyroData(int16_t * destination)
-{
-    uint8_t rawData[6];  // x/y/z gyro register data stored here
-    _readBytes(MPU9250_ADDRESS, GYRO_XOUT_H, 6, &rawData[0]);  // Read the six raw data registers sequentially into data array
-    destination[0] = ((int16_t)rawData[0] << 8) | rawData[1] ;  // Turn the MSB and LSB into a signed 16-bit value
-    destination[1] = ((int16_t)rawData[2] << 8) | rawData[3] ;  
-    destination[2] = ((int16_t)rawData[4] << 8) | rawData[5] ; 
-}
-
-static void readAccelData(int16_t * destination)
-{
-    uint8_t rawData[6];  // x/y/z accel register data stored here
-    _readBytes(MPU9250_ADDRESS, ACCEL_XOUT_H, 6, &rawData[0]);  // Read the six raw data registers into data array
-    destination[0] = ((int16_t)rawData[0] << 8) | rawData[1] ;  // Turn the MSB and LSB into a signed 16-bit value
-    destination[1] = ((int16_t)rawData[2] << 8) | rawData[3] ;  
-    destination[2] = ((int16_t)rawData[4] << 8) | rawData[5] ; 
-}
-
 EM7180 em7180;
+
+static const Ascale_t ASCALE     = AFS_2G;
+static const Gscale_t GSCALE     = GFS_250DPS;
+static const Mscale_t MSCALE     = MFS_16BITS;
+static const Mmode_t  MMODE      = M_100Hz;
+static const uint8_t SAMPLE_RATE_DIVISOR = 4;         
+
+// Instantiate MPU9250 class in master mode
+static MPU9250_Master mpu9250(ASCALE, GSCALE, MSCALE, MMODE, SAMPLE_RATE_DIVISOR);
+
+static void mpu9250_error(const char * errmsg) 
+{
+    Serial.print("MPU9250 Error: ");
+    Serial.println(errmsg);
+    while (true) ;
+}
+
+static void reportAcceleration(const char * dim, float val)
+{
+    Serial.print(dim);
+    Serial.print("-acceleration: ");
+    Serial.print(1000*val);
+    Serial.print(" milliG  "); 
+}
+
+static void reportGyroRate(const char * dim, float val)
+{
+    Serial.print(dim);
+    Serial.print("-gyro rate: ");
+    Serial.print(val, 1);
+    Serial.print(" degrees/sec  "); 
+}
+
+static void reportMagnetometer(const char * dim, float val)
+{
+    Serial.print(dim);
+    Serial.print("-magnetometer: ");
+    Serial.print(val);
+    Serial.print(" milligauss  "); 
+}
 
 void setup()
 {
-
     Serial.begin(115200);
 
 #ifdef __MK20DX256__
@@ -202,7 +81,7 @@ void setup()
 
     delay(100);
 
-    // Start the EM7180 in pass-through mode
+    // Start the EM7180
     if (!em7180.begin()) {
 
         while (true) {
@@ -210,42 +89,67 @@ void setup()
         }
     }    
 
-    // Set up for pass-through mode
+    // Put the EM7180 into pass-through mode
     em7180.setPassThroughMode();
 
-    // Read the WHO_AM_I register, this is a good test of communication
-    byte c = _readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);  // Read WHO_AM_I register for MPU-9250
-    if (c != 0x71) { // WHO_AM_I should always be 0x71
-        Serial.print("Could not connect to MPU9250: 0x");
-        Serial.println(c, HEX);
-        while(1) ; // Loop forever if communication doesn't happen
-    }
+    // Start the MPU9250 in master mode
+    switch (mpu9250.begin()) {
 
-    // Specify IMU scale
-    initMPU9250(AFS_2G, GFS_250DPS);
+        case MPU_ERROR_IMU_ID:
+            mpu9250_error("Bad IMU device ID");
+        case MPU_ERROR_MAG_ID:
+            mpu9250_error("Bad magnetometer device ID");
+        case MPU_ERROR_SELFTEST:
+            mpu9250_error("Failed self-test");
+        default:
+            Serial.println("MPU6050 online!\n");
+    }
 }
 
 
 void loop()
 {  
-    int16_t accelCount[3];  // Stores the 16-bit signed accelerometer sensor output
-    int16_t gyroCount[3];   // Stores the 16-bit signed gyro sensor output
+    static float ax, ay, az, gx, gy, gz, mx, my, mz, temperature;
 
-    readAccelData(accelCount);  // Read the x/y/z adc values
-    readGyroData(gyroCount);  // Read the x/y/z adc values
+    if (mpu9250.checkNewData())  { // data ready interrupt is detected
 
-    Serial.print(accelCount[0]);
-    Serial.print(" ");
-    Serial.print(accelCount[1]);
-    Serial.print(" ");
-    Serial.print(accelCount[2]);
-    Serial.print("   ");
-    Serial.print(gyroCount[0]);
-    Serial.print(" ");
-    Serial.print(gyroCount[1]);
-    Serial.print(" ");
-    Serial.print(gyroCount[2]);
-    Serial.println();
+        mpu9250.readAccelerometer(ax, ay, az);
+        mpu9250.readGyrometer(gx, gy, gz);
+        mpu9250.readMagnetometer(mx, my, mz);
+        temperature = mpu9250.readTemperature();
+    }
+
+    // Report at 4 Hz
+    static uint32_t msec_prev;
+    uint32_t msec_curr = millis();
+
+    if (msec_curr-msec_prev > 250) {
+
+        msec_prev = msec_curr;
+
+        reportAcceleration("X", ax);
+        reportAcceleration("Y", ay);
+        reportAcceleration("Z", az);
+
+        Serial.println();
+
+        reportGyroRate("X", gx);
+        reportGyroRate("Y", gy);
+        reportGyroRate("Z", gz);
+
+        Serial.println();;
+
+        reportMagnetometer("X", mx);
+        reportMagnetometer("Y", my);
+        reportMagnetometer("Z", mz);
+
+        Serial.println();;
+
+        // Print temperature in degrees Centigrade      
+        Serial.print("Gyro temperature is ");  
+        Serial.print(temperature, 1);  
+        Serial.println(" degrees C\n"); 
+    }
 }
 
 
