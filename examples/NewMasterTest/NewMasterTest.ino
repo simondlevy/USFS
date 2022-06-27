a8aac201dd76808ab102fe7d14e56cf8e46af337
simondlevy <simon.d.levy@gmail.com>
2022-06-27 19:31:23 -0400

    =More restoration work

--- examples/NewMasterTest/NewMasterTest.ino
+++ examples/NewMasterTest/NewMasterTest.ino
@@ -542,56 +542,6 @@ void accelgyrocalMPU9250(float * dest1, float * dest2)
     dest2[2] = (float)accel_bias[2]/(float)accelsensitivity;
 }
 
-
-void magcalMPU9250(float * dest1, float * dest2) 
-{
-    uint16_t ii = 0, sample_count = 0;
-    int32_t mag_bias[3] = {0, 0, 0}, mag_scale[3] = {0, 0, 0};
-    int16_t mag_max[3] = {0xFF, 0xFF, 0xFF}, mag_min[3] = {0x7F, 0x7F, 0x7F}, mag_temp[3] = {0, 0, 0};
-
-    Serial.println("Mag Calibration: Wave device in a figure eight until done!");
-    delay(4000);
-
-    if(Mmode == 0x02) sample_count = 128;
-    if(Mmode == 0x06) sample_count = 1500;
-    for(ii = 0; ii < sample_count; ii++) {
-        readMagData(mag_temp);  // Read the mag data   
-        for (int jj = 0; jj < 3; jj++) {
-            if(mag_temp[jj] > mag_max[jj]) mag_max[jj] = mag_temp[jj];
-            if(mag_temp[jj] < mag_min[jj]) mag_min[jj] = mag_temp[jj];
-        }
-        if(Mmode == 0x02) delay(135);  // at 8 Hz ODR, new mag data is available every 125 ms
-        if(Mmode == 0x06) delay(12);  // at 100 Hz ODR, new mag data is available every 10 ms
-    }
-
-    //    Serial.println("mag x min/max:"); Serial.println(mag_max[0]); Serial.println(mag_min[0]);
-    //    Serial.println("mag y min/max:"); Serial.println(mag_max[1]); Serial.println(mag_min[1]);
-    //    Serial.println("mag z min/max:"); Serial.println(mag_max[2]); Serial.println(mag_min[2]);
-
-    // Get hard iron correction
-    mag_bias[0]  = (mag_max[0] + mag_min[0])/2;  // get average x mag bias in counts
-    mag_bias[1]  = (mag_max[1] + mag_min[1])/2;  // get average y mag bias in counts
-    mag_bias[2]  = (mag_max[2] + mag_min[2])/2;  // get average z mag bias in counts
-
-    dest1[0] = (float) mag_bias[0]*mRes*magCalibration[0];  // save mag biases in G for main program
-    dest1[1] = (float) mag_bias[1]*mRes*magCalibration[1];   
-    dest1[2] = (float) mag_bias[2]*mRes*magCalibration[2];  
-
-    // Get soft iron correction estimate
-    mag_scale[0]  = (mag_max[0] - mag_min[0])/2;  // get average x axis max chord length in counts
-    mag_scale[1]  = (mag_max[1] - mag_min[1])/2;  // get average y axis max chord length in counts
-    mag_scale[2]  = (mag_max[2] - mag_min[2])/2;  // get average z axis max chord length in counts
-
-    float avg_rad = mag_scale[0] + mag_scale[1] + mag_scale[2];
-    avg_rad /= 3.0;
-
-    dest2[0] = avg_rad/((float)mag_scale[0]);
-    dest2[1] = avg_rad/((float)mag_scale[1]);
-    dest2[2] = avg_rad/((float)mag_scale[2]);
-
-    Serial.println("Mag Calibration done!");
-}
-
 int16_t readSENtralBaroData()
 {
     uint8_t rawData[2];  // x/y/z gyro register data stored here
