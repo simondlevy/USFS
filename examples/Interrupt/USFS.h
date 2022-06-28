47fbe3c21b0c2d7d5350c308c9ab82d04ce1dcbe
simondlevy <simon.d.levy@gmail.com>
2022-06-28 17:52:39 -0400

    =Cleanup

--- examples/Interrupt/USFS.h
+++ examples/Interrupt/USFS.h
@@ -33,43 +33,4 @@ class USFS
         void    readSENtralQuatData(float * destination);
         int16_t readSENtralTempData();
 
-    private:
-
-        bool _passThru;
-        uint8_t _intPin;
-        float _aRes;
-        float _gRes;
-        float _mRes;
-        uint8_t _Mmode;
-        float _fuseROMx;
-        float _fuseROMy;
-        float _fuseROMz;
-        float _q[4];
-        float _beta;
-        float _deltat;
-        float _Kp;
-        float _Ki;
-
-        float int32_reg_to_float (uint8_t *buf);
-        void float_to_bytes (float param_val, uint8_t *buf);
-        void EM7180_set_float_param (uint8_t param, float param_val);
-        void readAccelData(int16_t * destination);
-        void readGyroData(int16_t * destination);
-        void readMagData(int16_t * destination);
-        void initAK8963(uint8_t Mscale, uint8_t Mmode, float * destination);
-        void initMPU9250(uint8_t Ascale, uint8_t Gscale);
-        void accelgyrocalMPU9250(float * dest1, float * dest2);
-        void MPU9250SelfTest(float * destination);
-        void SENtralPassThroughMode();
-        void M24512DFMwriteByte(uint8_t device_address, uint8_t data_address1, uint8_t data_address2, uint8_t  data);
-        void M24512DFMwriteBytes(uint8_t device_address, uint8_t data_address1, uint8_t data_address2, uint8_t count, uint8_t * dest);
-        uint8_t M24512DFMreadByte(uint8_t device_address, uint8_t data_address1, uint8_t data_address2);
-        void M24512DFMreadBytes(uint8_t device_address, uint8_t data_address1, uint8_t data_address2, uint8_t count, uint8_t * dest);
-        void MS5637Reset();
-        void MS5637PromRead(uint16_t * destination);
-        unsigned char MS5637checkCRC(uint16_t * n_prom);
-        void MadgwickQuaternionUpdate(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz);
-        void MahonyQuaternionUpdate(float ax, float ay, float az, float gx, float gy, float gz, float mx, float my, float mz);
-        int16_t readTempData();
-
 };
