818dc5a4a1746421597f9665f4528e88b0143a4a
simondlevy <simon.d.levy@gmail.com>
2022-06-28 18:00:52 -0400

    =Cleanup

--- examples/Interrupt/Interrupt.ino
+++ examples/Interrupt/Interrupt.ino
@@ -1,3 +1,27 @@
+/* 
+   USFS interrupt example
+
+   Copyright (C) 2022 TleraCorp && Simon D. Levy
+
+   Adapted from
+
+     https://github.com/kriswiner/USFS_SENtral_sensor_hub/tree/master/WarmStartandAccelCal
+
+   This file is part of USFS.
+
+   USFS is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   USFS is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with USFS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <math.h>
 
 #include <Wire.h>
