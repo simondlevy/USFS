2bc99e1c8ea65ab467614810a33507c7253db316
simondlevy <simon.d.levy@gmail.com>
2024-01-18 23:45:55 -0500

    =More README

--- src/usfs_arduino.cpp
+++ src/usfs_arduino.cpp
@@ -3,21 +3,19 @@
 
    Copyright (C) 2024 Simon D. Levy
 
-https://github.com/kriswiner/SENtral_sensor_hub/tree/master/WarmStartandAccelCal
+   This file is part of USFS.
 
-This file is part of USFS.
+   USFS is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-USFS is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-USFS is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with USFS.  If not, see <http://www.gnu.org/licenses/>.
+   USFS is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with USFS.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <Arduino.h>
